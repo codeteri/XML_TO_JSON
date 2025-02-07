6717d9ad5000b948a977b79262a6f05970dbb05a
WAVYTERRY <codeeeterry@gmail.com>
2025-02-07 08:59:26 +0200

    feat(oldcode):pushed the old code

--- src/mapper.ts
+++ src/mapper.ts
@@ -1,4 +1,4 @@
-import { xml2json } from 'xml-js';
+""import { xml2json } from 'xml-js';
 
 export interface Invoice {
     ref: string;
@@ -48,57 +48,61 @@ export interface MapperInterface {
 
 export const Mapper = (): MapperInterface => {
 const ingestInvoice = (xml: string): Invoice => {
-    const jsonXml = JSON.parse(xml2json(xml, { compact: true }));
-    console.log(jsonXml.Invoice['cbc:ID']._text);
-    const invoice:Partial < Invoice > = {
-        ref: jsonXml.Invoice['cbc:ID']._text,
-        issued: jsonXml.Invoice['cbc:IssueDate']._text
-        // recipient: {
-        //     taxId: jsonXml.recipient.taxId._text,
-        //     name: jsonXml.recipient.name._text,
-        //     address: {
-        //         city: jsonXml.recipient.address.city._text,
-        //         country: jsonXml.recipient.address.country._text,
-        //         line1: jsonXml.recipient.address.line1._text,
-        //         line2: jsonXml.recipient.address.line2._text,
-        //         postalCode: jsonXml.recipient.address.postalCode._text,
-        //         state: jsonXml.recipient.address.state._text
-        //     }
-        // },
-        // lines: jsonXml.lines.map((line: any) => ({
-        //     name: line.name._text,
-        //     description: line.description._text,
-        //     quantity: line.quantity._text,
-        //     price: {
-        //         amount: line.price.amount._text
-        //     },
-        //     vat: {
-        //         amount: line.vat.amount._text,
-        //         type: line.vat.type._text,
-        //         code: line.vat.code._text,
-        //         exemptReason: line.vat.exemptReason._text
-        //     },
-        //     unit: line.unit._text
-        // })),
-        // total: {
-        //     amount: jsonXml.total.amount._text
-        // },
-        // customInfo: {
-        //     chn: {
-        //         invoiceCode: jsonXml.customInfo.chn.invoiceCode._text,
-        //         checkCode: jsonXml.customInfo.chn.checkCode._text
-        //     }
-        // }
-    };
-
-    // if (invoice.total.amount < 10000) {
-    //     invoice.customInfo = jsonXml.customInfo;
+const jsonXml = JSON.parse(xml2json(xml, { compact: true }));
+console.log(jsonXml.Invoice['cbc:ID']._text);
+const invoice:Partial < Invoice > = {
+    ref: jsonXml.Invoice['cbc:ID']._text,
+    issued: jsonXml.Invoice['cbc:IssueDate']._text,
+    // recipient: {
+    //     taxId: jsonXml.recipient.taxId._text,
+    //     name: jsonXml.recipient.name._text,
+    //     address: {
+    //         city: jsonXml.recipient.address.city._text,
+    //         country: jsonXml.recipient.address.country._text,
+    //         line1: jsonXml.recipient.address.line1._text,
+    //         line2: jsonXml.recipient.address.line2._text,
+    //         postalCode: jsonXml.recipient.address.postalCode._text,
+    //         state: jsonXml.recipient.address.state._text
+    //     }
+    // },
+    lines: jsonXml.Invoice['cac:InvoiceLine'].map((line: any) => {
+        console.log('line:', line);
+        console.log('line[\'cbc:Item\']:', line['cbc:Item']);
+        return {
+            name: line['cac:Item']['cbc:Name']._text,
+            description: line['cac:Item']['cbc:Description']._text,
+            quantity: line['cbc:InvoicedQuantity']._text,
+            price: {
+                amount: parseFloat(line['cac:Price']['cbc:PriceAmount']._text)
+            },
+            vat: {
+                amount: parseFloat(line['cac:TaxTotal']['cbc:TaxAmount']._text),
+                type: line['cac:TaxTotal']['cac:TaxSubtotal']['cac:TaxCategory']['cac:TaxScheme']['cbc:Name']._text,
+                code: line['cac:TaxTotal']['cac:TaxSubtotal']['cac:TaxCategory']['cac:TaxScheme']['cbc:ID']._text,
+                exemptReason: line['cac:TaxTotal']['cac:TaxSubtotal']['cac:TaxCategory']['cbc:TaxExemptionReason']?._text
+            },
+            unit: line['cbc:InvoicedQuantity']['@unitCode']
+        };
+    })
+    // total: {
+    //     amount: jsonXml.total.amount._text
+    // },
+    // customInfo: {
+    //     chn: {
+    //         invoiceCode: jsonXml.customInfo.chn.invoiceCode._text,
+    //         checkCode: jsonXml.customInfo.chn.checkCode._text
+    //     }
     // }
-    // @ts-ignore
-    return invoice;
+};
+
+// if (invoice.total.amount < 10000) {
+//     invoice.customInfo = jsonXml.customInfo;
+// }
+// @ts-ignore
+return invoice;
 };
 
 return {
     ingestInvoice
 };
-};
+};""
