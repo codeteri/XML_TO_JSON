e8164b1072ea7d4e7075b507cee5b7fec546ada2
sabbatj <kayeburgers@gmail.com>
2025-02-07 08:56:54 +0200

    fix(mapper): tested total

--- src/mapper.ts
+++ src/mapper.ts
@@ -1,4 +1,5 @@
 import { xml2json } from 'xml-js';
+
 export interface Invoice {
     ref: string;
     issued: string;
@@ -33,71 +34,71 @@ export interface Invoice {
     total: {
         amount: number;
     };
-    customInfo: {
+    customInfo?: {
         chn: {
             invoiceCode: string;
             checkCode: string;
         };
     };
 }
+
 export interface MapperInterface {
     ingestInvoice(xml: string): Invoice;
 }
+
 export const Mapper = (): MapperInterface => {
-const ingestInvoice = (xml: string): Invoice => {
-    const jsonXml = JSON.parse(xml2json(xml, { compact: true }));
-    console.log(jsonXml.Invoice['cbc:ID']._text);
-    const invoice:Partial < Invoice > = {
-        ref: jsonXml.Invoice['cbc:ID']._text,
-        issued: jsonXml.Invoice['cbc:IssueDate']._text,
-        // recipient: {
-        //     taxId: jsonXml.recipient.taxId._text,
-        //     name: jsonXml.recipient.name._text,
-        //     address: {
-        //         city: jsonXml.recipient.address.city._text,
-        //         country: jsonXml.recipient.address.country._text,
-        //         line1: jsonXml.recipient.address.line1._text,
-        //         line2: jsonXml.recipient.address.line2._text,
-        //         postalCode: jsonXml.recipient.address.postalCode._text,
-        //         state: jsonXml.recipient.address.state._text
-        //     }
-        // },
-        lines: jsonXml.Invoice['cac:InvoiceLine'].map((line: any) => {
-            console.log('line:', line);
-            console.log('line[\'cbc:Item\']:', line['cbc:Item']);
-            return {
+    const ingestInvoice = (xml: string): Invoice => {
+        const jsonXml = JSON.parse(xml2json(xml, { compact: true }));
+        
+        const invoice: Partial<Invoice> = {
+            ref: jsonXml.Invoice['cbc:ID']._text,
+            issued: jsonXml.Invoice['cbc:IssueDate']._text,
+            // recipient: {
+            //     taxId: jsonXml.Invoice['cac:AccountingCustomerParty']['cbc:CustomerAssignedAccountID']._text,
+            //     name: jsonXml.Invoice['cac:AccountingCustomerParty']['cac:Party']['cac:PartyName']['cbc:Name']._text,
+            //     mobile: jsonXml.Invoice['cac:AccountingCustomerParty']['cac:Contact']?.['cbc:Telephone']?._text || '',
+            //     address: {
+            //         city: jsonXml.Invoice['cac:AccountingCustomerParty']['cac:Party']['cac:PostalAddress']['cbc:CityName']._text,
+            //         country: jsonXml.Invoice['cac:AccountingCustomerParty']['cac:Party']['cac:PostalAddress']['cbc:Country']['cbc:IdentificationCode']._text,
+            //         line1: jsonXml.Invoice['cac:AccountingCustomerParty']['cac:Party']['cac:PostalAddress']['cbc:StreetName']._text,
+            //         line2: jsonXml.Invoice['cac:AccountingCustomerParty']['cac:Party']['cac:PostalAddress']['cbc:AdditionalStreetName']?._text || '',
+            //         postalCode: jsonXml.Invoice['cac:AccountingCustomerParty']['cac:Party']['cac:PostalAddress']['cbc:PostalZone']._text,
+            //         state: jsonXml.Invoice['cac:AccountingCustomerParty']['cac:Party']['cac:PostalAddress']['cbc:CountrySubentity']._text
+            //     }
+            // },
+            lines: jsonXml.Invoice['cac:InvoiceLine'].map((line: any) => ({
                 name: line['cac:Item']['cbc:Name']._text,
-                description: line['cac:Item']['cbc:Description']._text,
+                description: line['cac:Item']['cbc:Description']?._text || '',
                 quantity: line['cbc:InvoicedQuantity']._text,
                 price: {
                     amount: parseFloat(line['cac:Price']['cbc:PriceAmount']._text)
                 },
                 vat: {
                     amount: parseFloat(line['cac:TaxTotal']['cbc:TaxAmount']._text),
-                    type: line['cac:TaxTotal']['cac:TaxSubtotal']['cac:TaxCategory']['cac:TaxScheme']['cbc:Name']._text,
-                    code: line['cac:TaxTotal']['cac:TaxSubtotal']['cac:TaxCategory']['cac:TaxScheme']['cbc:ID']._text,
-                    exemptReason: line['cac:TaxTotal']['cac:TaxSubtotal']['cac:TaxCategory']['cbc:TaxExemptionReason']?._text
+                    type: line['cac:TaxTotal']['cac:TaxSubtotal']['cac:TaxCategory']['cac:TaxScheme']['cbc:ID']._text,
+                    code: line['cac:TaxTotal']['cac:TaxSubtotal']['cac:TaxCategory']['cbc:ID']._text,
+                    exemptReason: line['cac:TaxTotal']['cac:TaxSubtotal']['cac:TaxCategory']['cbc:TaxExemptionReason']?._text || ''
                 },
-                unit: line['cbc:InvoicedQuantity']['@unitCode']
-            };
-        })
-        // total: {
-        //     amount: jsonXml.total.amount._text
-        // },
-        // customInfo: {
-        //     chn: {
-        //         invoiceCode: jsonXml.customInfo.chn.invoiceCode._text,
-        //         checkCode: jsonXml.customInfo.chn.checkCode._text
-        //     }
+                unit: line['cbc:InvoicedQuantity'].attributes?.unitCode || ''
+            })),
+            total: {
+                amount: jsonXml.Invoice['cac:LegalMonetaryTotal']['cbc:PayableAmount']?._text ? parseFloat(jsonXml.Invoice['cac:LegalMonetaryTotal']['cbc:PayableAmount']._text) : 0
+            }
+            // customInfo: {
+            //     chn: {
+            //         invoiceCode: jsonXml.Invoice['cbc:InvoiceTypeCode']?._text || '',
+            //         checkCode: jsonXml.Invoice['cac:Signature']['cbc:ID']?._text || ''
+            //     }
+            // }
+        };
+
+        // if (invoice.total!.amount < 10000) {
+        //     invoice.customInfo = jsonXml.customInfo;
         // }
+        
+        // @ts-ignore
+        return invoice as Invoice;
     };
-    // if (invoice.total.amount < 10000) {
-    //     invoice.customInfo = jsonXml.customInfo;
-    // }
-    // @ts-ignore
-    return invoice;
-};
-return {
-    ingestInvoice
-};
+
+    return { ingestInvoice };
 };
